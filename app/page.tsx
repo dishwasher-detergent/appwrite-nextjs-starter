a966d33e7c43bd39dbcee1dcf4d51caa93fac7f0
Kenneth Bass <kenny_bass@outlook.com>
2025-03-08 12:44:19 -0600

    test

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,9 @@
 import Link from "next/link";
 
 import { Button } from "@/components/ui/button";
-import { getLoggedInUser } from "@/lib/auth";
 
 export default async function Home() {
-  const user = await getLoggedInUser();
+  // const user = await getLoggedInUser();
 
   return (
     <>
@@ -15,7 +14,7 @@ export default async function Home() {
           </h1>
           <div className="flex items-center gap-2">
             {/* <ModeToggle /> */}
-            {user ? (
+            {/* {user ? (
               <Button size="sm" asChild>
                 <Link href="/app">Go to App</Link>
               </Button>
@@ -23,7 +22,10 @@ export default async function Home() {
               <Button size="sm" asChild>
                 <Link href="/signup">Sign Up</Link>
               </Button>
-            )}
+            )} */}
+            <Button size="sm" asChild>
+              <Link href="/signup">Sign Up</Link>
+            </Button>
           </div>
         </nav>
       </header>
