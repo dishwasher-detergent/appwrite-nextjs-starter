4c395e254e9c625ad4c1343086248f4c726a207e
Kenneth Bass <kenny_bass@outlook.com>
2025-03-08 13:31:29 -0600

    add text for no samples

--- app/(app)/app/page.tsx
+++ app/(app)/app/page.tsx
@@ -7,14 +7,19 @@ export default async function AppPage() {
 
   return (
     <>
-      <header className="flex flex-row justify-between items-center pb-4">
+      <header className="flex flex-row justify-between items-center pb-4 w-full">
         <h2 className="font-bold">Samples</h2>
         <AddSample />
       </header>
-      <section className="min-h-full columns-xs items-start gap-4 space-y-4">
+      <section className="min-h-full columns-xs items-start gap-4 space-y-4 w-full">
         {data?.documents?.map((sample) => (
           <SampleCard key={sample.$id} {...sample} />
         ))}
+        {data?.documents?.length === 0 && (
+          <p className="font-semibold text-muted-foreground">
+            No samples found
+          </p>
+        )}
       </section>
     </>
   );
