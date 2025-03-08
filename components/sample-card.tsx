af1623d9875210c0ca8d334dd9367b3f27cda320
Kenneth Bass <kenny_bass@outlook.com>
2025-03-07 23:44:18 -0600

    small styling update

--- components/sample-card.tsx
+++ components/sample-card.tsx
@@ -17,26 +17,32 @@ import Image from "next/image";
 
 export function SampleCard(sample: Sample) {
   return (
-    <Card className="break-inside-avoid-column rounded-md">
-      <CardContent className="space-y-2">
-        {sample.image && (
+    <Card className="break-inside-avoid-column rounded-md py-0 gap-0">
+      <CardContent className="p-0 border-b">
+        {sample.image ? (
           <Image
             src={`${ENDPOINT}/storage/buckets/${SAMPLE_BUCKET_ID}/files/${sample.image}/view?project=${PROJECT_ID}`}
             alt={sample.name}
-            className="rounded-md object-cover bg-primary aspect-square"
+            className="object-cover bg-primary aspect-square rounded-t-md"
             width={500}
             height={500}
             sizes="(max-width: 500px)"
           />
+        ) : (
+          <div className="w-full aspect-square bg-muted rounded-t-md">
+            <p className="flex h-full items-center justify-center text-muted-foreground font-semibold">
+              No image
+            </p>
+          </div>
         )}
       </CardContent>
-      <CardHeader>
+      <CardHeader className="p-4">
         <CardTitle className="truncate">{sample.name}</CardTitle>
         <CardDescription className="truncate">
           {sample.description}
         </CardDescription>
       </CardHeader>
-      <CardFooter className="flex flex-row justify-end gap-1">
+      <CardFooter className="flex flex-row justify-end gap-1 p-4">
         <DeleteSample sample={sample} />
         <EditSample sample={sample} />
       </CardFooter>
