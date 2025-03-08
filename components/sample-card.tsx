ece9eef6afd0413739abed1fd4e1dec5d20ef6e9
Kenneth Bass <kenny_bass@outlook.com>
2025-03-08 10:48:37 -0600

    restyle card

--- components/sample-card.tsx
+++ components/sample-card.tsx
@@ -3,55 +3,56 @@
 import Image from "next/image";
 import Link from "next/link";
 
-import { DeleteSample } from "@/components/delete-sample";
-import { EditSample } from "@/components/edit-sample";
 import { Button } from "@/components/ui/button";
 import {
   Card,
   CardContent,
   CardDescription,
-  CardFooter,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
 import { Sample } from "@/interfaces/sample.interface";
 import { ENDPOINT, PROJECT_ID, SAMPLE_BUCKET_ID } from "@/lib/constants";
+import { LucideLink } from "lucide-react";
 
 export function SampleCard(sample: Sample) {
   return (
-    <Card className="break-inside-avoid-column rounded-md py-0 gap-0">
-      <CardContent className="p-0 border-b">
+    <Card className="break-inside-avoid-column rounded-md overflow-hidden py-0 gap-0 ">
+      <CardContent className="p-0 relative">
         {sample.image ? (
           <Image
             src={`${ENDPOINT}/storage/buckets/${SAMPLE_BUCKET_ID}/files/${sample.image}/view?project=${PROJECT_ID}`}
             alt={sample.name}
-            className="object-cover bg-primary rounded-t-md"
+            className="object-cover bg-primary"
             width={500}
-            height={250}
+            height={500}
             sizes="(max-width: 500px)"
           />
         ) : (
-          <div className="w-full aspect-video bg-muted rounded-t-md">
+          <div className="w-full aspect-video bg-muted">
             <p className="flex h-full items-center justify-center text-muted-foreground font-semibold">
               No image
             </p>
           </div>
         )}
+        <CardHeader className="flex flex-col justify-end bottom-0 absolute w-full p-4 h-full bg-linear-to-t from-primary to-transparent">
+          <CardTitle className="text-primary-foreground">
+            <Button
+              className="truncate p-0! text-primary-foreground text-xl"
+              variant="link"
+              asChild
+            >
+              <Link href={`/app/sample/${sample.$id}`}>
+                {sample.name}
+                <LucideLink />
+              </Link>
+            </Button>
+          </CardTitle>
+          <CardDescription className="text-primary-foreground line-clamp-3">
+            {sample?.description ?? "No description provided."}
+          </CardDescription>
+        </CardHeader>
       </CardContent>
-      <CardHeader className="p-4">
-        <CardTitle>
-          <Button className="truncate p-0" variant="link" asChild>
-            <Link href={`/app/sample/${sample.$id}`}>{sample.name}</Link>
-          </Button>
-        </CardTitle>
-        <CardDescription className="truncate">
-          {sample?.description ?? "No description provided."}
-        </CardDescription>
-      </CardHeader>
-      <CardFooter className="flex flex-row justify-end gap-1 p-4">
-        <DeleteSample sample={sample} />
-        <EditSample sample={sample} />
-      </CardFooter>
     </Card>
   );
 }
