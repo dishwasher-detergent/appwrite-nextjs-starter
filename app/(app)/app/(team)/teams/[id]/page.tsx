6f72add0f77ad77ec7737350fb6b8247d3749dfb
Kenneth Bass <33816169+dishwasher-detergent@users.noreply.github.com>
2025-03-17 10:39:51 -0500

    Update page.tsx

--- app/(app)/app/(team)/teams/[id]/page.tsx
+++ app/(app)/app/(team)/teams/[id]/page.tsx
@@ -6,10 +6,10 @@ export default async function TeamPage({
   params: Promise<{ id: string }>;
 }) {
   const { id: teamId } = await params;
-  const { data, success } = await getTeamById(teamId);
+  const { data, success, message } = await getTeamById(teamId);
 
   if (!success || !data) {
-    return <p>Failed</p>
+    return <p>{message}</p>
   }
 
   return <p>{data.name}</p>;
