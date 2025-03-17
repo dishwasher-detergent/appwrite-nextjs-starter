fa974bbcc4d670b876f5d63f4fc3cd737bde790c
Kenneth Bass <33816169+dishwasher-detergent@users.noreply.github.com>
2025-03-17 13:58:42 -0500

    fix styling

--- components/team-card.tsx
+++ components/team-card.tsx
@@ -35,10 +35,10 @@ export function TeamCard(team: TeamData) {
             />
             <AvatarFallback className="rounded-lg">{getInitials(team.name)}</AvatarFallback>
         </Avatar>
-        <CardHeader className="flex flex-col justify-end w-full h-full p-2">
+        <CardHeader className="flex flex-col justify-end w-full h-full px-2">
           <CardTitle>
             <Button
-              className="truncate p-0! text-lg"
+              className="truncate p-0! text-lg h-auto"
               variant="link"
               asChild
             >
