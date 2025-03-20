84f395b6653f2696319a0fd5ddfc594a8c822b98
Kenneth Bass <kenny_bass@outlook.com>
2025-03-20 12:20:43 -0500

    temp fix

--- app/(auth)/accept/[id]/page.tsx
+++ app/(auth)/accept/[id]/page.tsx
@@ -1,16 +1,4 @@
-import { AspectRatio } from "@/components/ui/aspect-ratio";
-import Image from "next/image";
-import { redirect } from "next/navigation";
-
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-import { AVATAR_BUCKET_ID, ENDPOINT, PROJECT_ID } from "@/lib/constants";
-import { getTeamById } from "@/lib/team";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { AcceptForm } from "./form";
 
 export default async function Invite({
@@ -28,29 +16,10 @@ export default async function Invite({
   const { id } = await params;
   const { teamId, membershipId, userId, secret } = await searchParams;
 
-  const { data } = await getTeamById(id);
-
-  if (!data) {
-    redirect("/app");
-  }
-
   return (
     <Card className="w-full max-w-sm">
       <CardHeader>
-        {data?.avatar && (
-          <AspectRatio ratio={1} className="w-full overflow-hidden rounded-lg">
-            <Image
-              src={`${ENDPOINT}/storage/buckets/${AVATAR_BUCKET_ID}/files/${data.avatar}/view?project=${PROJECT_ID}`}
-              alt={data?.name}
-              className="object-cover object-left-top bg-primary"
-              fill
-            />
-          </AspectRatio>
-        )}
-        <CardTitle className="text-2xl mt-2">Join {data?.name}</CardTitle>
-        <CardDescription>
-          Accept Invite to join the team {data?.name}
-        </CardDescription>
+        <CardTitle className="text-2xl mt-2">Accept Team Invite</CardTitle>
       </CardHeader>
       <CardContent>
         <AcceptForm
