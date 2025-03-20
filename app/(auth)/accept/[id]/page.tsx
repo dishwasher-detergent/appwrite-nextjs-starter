c9e66e3f426deb5a2503b1a034f1a5882ff7cc64
Kenneth Bass <kenny_bass@outlook.com>
2025-03-20 12:21:07 -0500

    temp fix

--- app/(auth)/accept/[id]/page.tsx
+++ app/(auth)/accept/[id]/page.tsx
@@ -2,10 +2,8 @@ import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { AcceptForm } from "./form";
 
 export default async function Invite({
-  params,
   searchParams,
 }: {
-  params: Promise<{ id: string }>;
   searchParams: Promise<{
     teamId: string;
     membershipId: string;
@@ -13,7 +11,6 @@ export default async function Invite({
     secret: string;
   }>;
 }) {
-  const { id } = await params;
   const { teamId, membershipId, userId, secret } = await searchParams;
 
   return (
