4537a12d3c00555099525f403e68d71b28ff0671
Kenneth Bass <33816169+dishwasher-detergent@users.noreply.github.com>
2025-03-25 13:41:38 -0500

    test

--- app/(app)/app/(team)/teams/loading.tsx
+++ app/(app)/app/(team)/teams/loading.tsx
@@ -1,4 +1,5 @@
-import { Skeleton } from "@/components/ui";
+import { TeamCardLoading } from "@/components/team/team-card-loading";
+import { Skeleton } from "@/components/ui/skeleton";
 
 export default function Loading() {
   return (
@@ -7,14 +8,7 @@ export default function Loading() {
         <h2 className="font-bold">Your Teams</h2>
         <Skeleton className="w-[120px] h-10" />
       </header>
-      <section className="grid grid-cols-1 md:grid-cols-3 gap-4">
-        {Array.from({ length: 6 }).map((_, index) => (
-          <Skeleton
-            key={index}
-            className="w-full h-[120px] rounded-lg"
-          />
-        ))}
-      </section>
+      <TeamCardLoading />
     </>
   );
 }
